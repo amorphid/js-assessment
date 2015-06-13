392303c79f8ba554c847f0b4034d332500eb85a4
Michael Pope <mpope.cr@gmail.com>
2015-06-13 15:46:13 -0400

    Implement asyncAnswers.manipulateRemoteData

--- app/async.js
+++ app/async.js
@@ -1,5 +1,7 @@
 exports = (typeof window === 'undefined') ? global : window;
 
+
+
 exports.asyncAnswers = {
   async : function(value) {
     return {
@@ -10,6 +12,19 @@ exports.asyncAnswers = {
   },
 
   manipulateRemoteData : function(url) {
+    var promise = new Promise(function (resolve, reject) {
+      var request = new XMLHttpRequest();
+      request.open("GET", url);
+      request.onload = function () {
+        var people = JSON.parse(this.response).people.map(function (i) {
+          return i.name;
+        })
 
+        resolve(people.sort());
+      };
+      request.send();
+    });
+
+    return promise;
   }
 };
