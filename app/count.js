e2f3aaf7ddf5e493a3231149d232447693515d67
Michael Pope <mpope.cr@gmail.com>
2015-06-14 08:25:02 -0400

    Implement countAnswers.count.cancel

--- app/count.js
+++ app/count.js
@@ -2,17 +2,24 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
-    var count = start;
+    var count     = start;
+    var timeout;
 
     var funktion = function () {
       console.log(count);
       count += 1;
 
       if (count <= end) {
-        setTimeout(funktion, 100);
+        timeout = setTimeout(funktion, 100);
       }
     }
 
     funktion();
+
+    return {
+      cancel: function () {
+        timeout && clearTimeout(timeout);
+      }
+    }
   }
 };
