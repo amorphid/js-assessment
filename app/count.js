d1b98749876e14fb67d86e79d24cc044e6c30e0b
Michael Pope <mpope.cr@gmail.com>
2015-06-14 08:18:46 -0400

    Implement countAnswers.count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,17 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var count = start;
 
+    var funktion = function () {
+      console.log(count);
+      count += 1;
+
+      if (count <= end) {
+        setTimeout(funktion, 100);
+      }
+    }
+
+    funktion();
   }
 };
